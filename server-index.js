eea4147310e4d4734a342b4dfa59306f4ca5025b
petehunt <floydophone@gmail.com>
2013-10-04 11:48:25 -0700

    first pass at ReactHackServer

--- server-index.js
+++ server-index.js
@@ -1,1 +1,8 @@
-module.exports = 'Nothing to see here... yet.';
+var FetchingMixin = require('./lib/FetchingMixin');
+var ReactHackServer = require('./lib/ReactHackServer');
+
+ReactHackServer.FetchingMixin = FetchingMixin;
+
+require('node-jsx').install();
+
+module.exports = ReactHackServer;
